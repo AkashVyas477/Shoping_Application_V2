425409e2bbebf4617166bc796b96d134c1e2c207
studiomac-192 <studiomac-192@studiomac-192s-iMac.local>
2022-03-22 12:42:46 +0530

    total price  mistak in cart screen

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -43,7 +43,7 @@ const CartScreen = props => {
       <Card style={styles.summary}>
         <Text style={styles.summaryText}>
           Total:{' '}
-          <Text style={styles.amount}>${Math.round(cartTotalAmount.toFixed(2)*100)}</Text>
+          <Text style={styles.amount}>${Math.round(cartTotalAmount.toFixed(2)*100)/100}</Text>
         </Text>
         {isLoading ? (<ActivityIndicator size='small' color={Colors.secondary} />):(
         <Button
